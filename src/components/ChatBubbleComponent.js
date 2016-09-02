f8c9dc9158f94e334fb40f97f0191225ce6af223
Casaper <vollenweider@gmail.com>
2016-09-02 17:11:15 +0200

    initial stuff

---
+++ src/components/ChatBubbleComponent.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/ChatBubble.css');
+
+class ChatBubbleComponent extends React.Component {
+  render() {
+    return (
+      <div className="chatbubble-component">
+        Please edit src/components///ChatBubbleComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+ChatBubbleComponent.displayName = 'ChatBubbleComponent';
+
+// Uncomment properties you need
+ChatBubbleComponent.propTypes = {
+};
+// ChatBubbleComponent.defaultProps = {};
+
+export default ChatBubbleComponent;
