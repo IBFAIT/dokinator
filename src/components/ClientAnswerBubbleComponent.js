a0f28b124afc45f115d56795ba5d52c8263a52fa
Casaper <vollenweider@gmail.com>
2016-09-16 21:06:12 +0200

    finito today

---
+++ src/components/ClientAnswerBubbleComponent.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React from 'react';
+
+import IconComponent from './IconComponent.js';
+
+const ClientAnswerBubble = ({text,name='DefaultName', avatar}) => (
+  <div className="clientbuttonpast-component">
+  <div>
+    <IconComponent name={name} src={avatar.src} alt={avatar.alt} />
+    <div className="text">
+      { text }
+    </div>
+    </div>
+  </div>
+);
+
+ClientAnswerBubble.displayName = 'ClientAnswerBubble';
+
+// Uncomment properties you need
+// ClientButtonComponent.propTypes = {};
+// ClientButtonComponent.defaultProps = {};
+
+export default ClientAnswerBubble;
