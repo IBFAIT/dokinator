ccd307ebcf987d1a9bf7f4d9ebfeecf8f028bcaa
Casaper <vollenweider@gmail.com>
2016-09-28 13:33:38 +0200

    feat: first step to random answer

--- src/components/BotSingleBubbleComponent.js
+++ src/components/BotSingleBubbleComponent.js
@@ -3,9 +3,14 @@
 import React from 'react';
 
 
-let BotSingleBubbleComponent = ({text}) => (
+let BotSingleBubbleComponent = ({text, data}) => (
   <div className="botsinglebubble-component">
-    <div className="text">{eval('`'+text+'`')}</div>
+  { (typeof text == 'array') ?
+    text.map((txt, key) => (<div key={key} className="text">{eval('`'+txt+'`')}</div>))
+    :
+    [text].map((txt, key) => (<div key={key} className="text">{eval('`'+txt+'`')}</div>))
+  }
+
   </div>
 );
 
