9cfd7c1c399feff349ea4056ce899914f8cfe870
Casaper <vollenweider@gmail.com>
2016-11-10 23:20:58 +0100

    fix: updated the new vals names

--- src/components/UserButton.js
+++ src/components/UserButton.js
@@ -9,14 +9,13 @@ const typeStyl = {
   default: {
     display: 'inline-block',
     margin: '0.5rem 0.2rem',
-    padding: vals.bubblePadding,
-    borderRadius: vals.bubbleBorderRadius,
-    boxShadow: '0.03rem 0.07rem 0 0 rgba(180, 180, 180, 0.5)',
-    backgroundColor: vals.bubbleBg
+    padding: vals.bubble.padding.top,
+    borderRadius: vals.bubble.border.radius,
+    boxShadow: vals.bubble.boxShadow,
+    backgroundColor: vals.bubble.background.color
   },
   disabled: {
-    color: 'rgba(0,0,0, 0.2)',
-    boxShadow: '0.03rem 0.07rem 0 0 rgba(180, 180, 180, 0.1)'
+    color: 'rgba(0,0,0, 0.2)'
   },
   past: {
     alignSelf: 'flex-end'
@@ -28,7 +27,7 @@ const UserButton = ({updatePathState, text, index, path, type = null}) => {
 
   // Styles for disabled buttons
   Style = (type === 'disabled') ? {...Style, ...typeStyl.disabled} : Style;
-  Style = (type === 'past') ? {...Style, ...typeStyl.past } : Style;
+  Style = (type === 'past') ? {...Style, ...typeStyl.past} : Style;
   // dynprops
   let dynProps = (type === null) ? {onClick: e => {updatePathState(e, {path, index})}} : {};
 
