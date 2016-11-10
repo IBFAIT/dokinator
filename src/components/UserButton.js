1302eb273042c7584cccea9fc2acfa005bdd272d
Casaper <vollenweider@gmail.com>
2016-11-10 14:05:06 +0100

    chore: next inline style step

--- src/components/UserButton.js
+++ src/components/UserButton.js
@@ -2,6 +2,14 @@
 
 import React from 'react';
 
+// generic Styles
+import genStyl from '../styles/genStyl.js';
+// Component styles
+const Style = {
+  display: 'inline-block',
+  minWidth: '10%'
+}
+
 const UserButton = ({updatePathState, text, index, path}) => (
   <div className="user-button">
     <button {...{
