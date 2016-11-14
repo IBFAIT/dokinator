f56026d339c011c54556750be5ca7d6723d74f4f
Casaper <vollenweider@gmail.com>
2016-11-14 16:48:23 +0100

    style: minor wrong comma

--- src/components/BotPart.js
+++ src/components/BotPart.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const avatarImgs = {
   doc:  require('../images/docinator.jpg'),
-  fred: require('../images/fred.png'),
+  fred: require('../images/fred.png')
 }
 
 const BotPart = ({children, botIdentity, type}) => {
