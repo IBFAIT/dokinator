2d0969c1bbe0072ba17e81f83853e736fe8bef68
Casaper <vollenweider@gmail.com>
2016-11-17 15:44:11 +0100

    chore(review): remove commented config option
    - there was a problem with react hot loader

--- cfg/dev.js
+++ cfg/dev.js
@@ -11,13 +11,13 @@ let BowerWebpackPlugin = require('bower-webpack-plugin');
 let config = Object.assign({}, baseConfig, {
   entry: [
     'webpack-dev-server/client?http://127.0.0.1:' + defaultSettings.port,
-    //'webpack/hot/only-dev-server', // commented because hot loading did not work anymore
+    'webpack/hot/only-dev-server', // commented because hot loading did not work anymore
     './src/index'
   ],
   cache: true,
   devtool: 'eval-source-map',
   plugins: [
-  //  new webpack.HotModuleReplacementPlugin(),
+   new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new BowerWebpackPlugin({
       searchResolveModulesDirectories: false
@@ -29,11 +29,7 @@ let config = Object.assign({}, baseConfig, {
 // Add needed loaders to the defaults here
 config.module.loaders.push({
   test: /\.(js|jsx)$/,
-
-  // trying to get over some hot loader trouble that seems
-  // to be in react-hot-loader at the moment
-  loader: 'babel-loader',
-  // loader: 'react-hot!babel-loader',    // old original loader
+  loader: 'react-hot!babel-loader',    // old original loader
 
   include: [].concat(
     config.additionalPaths,
