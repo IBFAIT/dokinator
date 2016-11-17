43280e11f7a30eeadeb855f8dd28e884052a0d8d
Casaper <vollenweider@gmail.com>
2016-11-17 22:29:46 +0100

    chore: removed obsolete comment

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -41,6 +41,14 @@ function getDefaultModules() {
         loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
       },
       {
+        test: /\.less/,
+        loader: 'style-loader!css-loader!less-loader'
+      },
+      {
+        test: /\.styl/,
+        loader: 'style-loader!css-loader!stylus-loader'
+      },
+      {
         test: /\.(png|jpg|gif|woff|woff2)$/,
         loader: 'url-loader?limit=8192'
       },
--- cfg/dev.js
+++ cfg/dev.js
@@ -11,13 +11,13 @@ let BowerWebpackPlugin = require('bower-webpack-plugin');
 let config = Object.assign({}, baseConfig, {
   entry: [
     'webpack-dev-server/client?http://127.0.0.1:' + defaultSettings.port,
-    'webpack/hot/only-dev-server', // commented because hot loading did not work anymore
+    'webpack/hot/only-dev-server',
     './src/index'
   ],
   cache: true,
   devtool: 'eval-source-map',
   plugins: [
-   new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new BowerWebpackPlugin({
       searchResolveModulesDirectories: false
@@ -29,7 +29,7 @@ let config = Object.assign({}, baseConfig, {
 // Add needed loaders to the defaults here
 config.module.loaders.push({
   test: /\.(js|jsx)$/,
-  loader: 'react-hot!babel-loader',    // old original loader
+  loader: 'react-hot!babel-loader',
 
   include: [].concat(
     config.additionalPaths,
